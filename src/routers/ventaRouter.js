d04c88afbcc91390073280133b4ba9dd094077b0
mamenendez <mamenendez@gmail.com>
2022-06-01 22:47:22 -0300

    saco opciones del menu que no van , mejoras en seccion mascotas , carrito de compras mismo nivel que opciones de menu

--- src/routers/ventaRouter.js
+++ src/routers/ventaRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const ventaController = require('../controllers/ventaControllers');
 
 router.get('/',ventaController.index);
-router.get('/marcas',ventaController.marcas);
-router.get('/promociones',ventaController.promociones);
+// router.get('/marcas',ventaController.marcas);
+// router.get('/promociones',ventaController.promociones);
 router.get('/productCart',ventaController.productCart);
 router.get('/productDetail',ventaController.productDetail);
 router.get('/sucursales',ventaController.sucursales);
